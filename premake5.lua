37ce8cc85c9c278fc316a45e0a97c007766fc280
Andrew Depke <andrewdepke@gmail.com>
2019-12-07 02:45:18 -0700

    Updated Build Script

--- premake5.lua
+++ premake5.lua
@@ -14,18 +14,20 @@ project "Engine"
 	targetdir "Build/Bin/%{cfg.platform}_%{cfg.buildcfg}"
 	
 	targetname "Vanguard"
-	
-	filter { "platforms:Win64" }
-		system "Windows"
 		
 	filter {}
+		defines { "PLATFORM_WINDOWS=0", "BUILD_DEBUG=0", "BUILD_DEVELOPMENT=0", "BUILD_RELEASE=0" }
 		flags { "FatalWarnings", "NoPCH", "MultiProcessorCompile" }
 		clr "Off"
 		rtti "Off"
 		characterset "Unicode"
 		
+	filter { "platforms:Win64" }
+		system "Windows"
+		defines { "PLATFORM_WINDOWS=1" }
+		
 	filter { "configurations:Debug" }
-		defines { "DEBUG", "D3DCOMPILE_DEBUG=1" }
+		defines { "BUILD_DEBUG=1", "DEBUG", "D3DCOMPILE_DEBUG=1" }
 		flags { }
 		symbols "On"
 		optimize "Off"
@@ -33,7 +35,7 @@ project "Engine"
 		exceptionhandling "On"
 		
 	filter { "configurations:Development" }
-		defines { "DEBUG", "JOBS_DISABLE_LOGGING" }
+		defines { "BUILD_DEVELOPMENT=1", "DEBUG", "JOBS_DISABLE_LOGGING" }
 		flags { "LinkTimeOptimization" }
 		symbols "On"
 		optimize "Debug"
@@ -41,7 +43,7 @@ project "Engine"
 		exceptionhandling "On"
 		
 	filter { "configurations:Release" }
-		defines { "NDEBUG", "JOBS_DISABLE_LOGGING" }
+		defines { "BUILD_RELEASE=1", "NDEBUG", "JOBS_DISABLE_LOGGING" }
 		flags { "LinkTimeOptimization", "NoRuntimeChecks" }
 		symbols "Off"
 		optimize "Full"
@@ -49,4 +51,17 @@ project "Engine"
 		exceptionhandling "Off"
 		
 	filter {}
-		files { "VanguardEngine/**.h", "VanguardEngine/**.cpp" }
+		files { "VanguardEngine/Source/Core/*.h", "VanguardEngine/Source/Core/*.cpp" }
+		files { "VanguardEngine/Source/Debug/*.h", "VanguardEngine/Source/Debug/*.cpp" }
+		files { "VanguardEngine/Source/Editor/*.h", "VanguardEngine/Source/Editor/*.cpp" }
+		files { "VanguardEngine/Source/Rendering/*.h", "VanguardEngine/Source/Rendering/*.cpp" }
+		files { "VanguardEngine/Source/Threading/*.h", "VanguardEngine/Source/Threading/*.cpp" }
+		files { "VanguardEngine/Source/Utility/*.h", "VanguardEngine/Source/Utility/*.cpp" }
+		
+	filter { "platforms:Win64" }
+		files { "VanguardEngine/Source/Core/Windows/*.h", "VanguardEngine/Source/Core/Windows/*.cpp" }
+		files { "VanguardEngine/Source/Debug/Windows/*.h", "VanguardEngine/Source/Debug/Windows/*.cpp" }
+		files { "VanguardEngine/Source/Editor/Windows/*.h", "VanguardEngine/Source/Editor/Windows/*.cpp" }
+		files { "VanguardEngine/Source/Rendering/Windows/*.h", "VanguardEngine/Source/Rendering/Windows/*.cpp" }
+		files { "VanguardEngine/Source/Threading/Windows/*.h", "VanguardEngine/Source/Threading/Windows/*.cpp" }
+		files { "VanguardEngine/Source/Utility/Windows/*.h", "VanguardEngine/Source/Utility/Windows/*.cpp" }
