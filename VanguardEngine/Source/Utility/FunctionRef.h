6d592b33552fe4c352496b7a17073869c5e52087
Andrew Depke <andrewdepke@gmail.com>
2019-12-07 17:45:45 -0700

    Added Function Ref

---
+++ VanguardEngine/Source/Utility/FunctionRef.h
@@ -0,0 +1,42 @@
+// Copyright (c) 2019 Andrew Depke
+
+#pragma once
+
+#include <functional>
+#include <type_traits>
+
+template <typename>
+struct FunctionRef;
+
+template <typename Result, typename... ArgTypes>
+struct FunctionRef<Result(ArgTypes...)>
+{
+private:
+	void* Object = nullptr;
+	Result(*Function)(void*, ArgTypes...) = nullptr;
+
+public:
+	constexpr FunctionRef() noexcept = delete;
+	template <typename Functor>
+	constexpr FunctionRef(Functor&& Func) noexcept : Object(const_cast<void*>(reinterpret_cast<const void*>(std::addressof(Func))))
+	{
+		Function = +[](void* Object, ArgTypes... Args)
+		{
+			return std::invoke(*reinterpret_cast<std::add_pointer_t<Functor>>(Object), std::forward<ArgTypes>(Args)...);
+		};
+	}
+	constexpr FunctionRef(const FunctionRef&) noexcept = default;
+	constexpr FunctionRef(FunctionRef&&) noexcept = default;
+
+	constexpr FunctionRef& operator=(const FunctionRef&) noexcept = default;
+	constexpr FunctionRef& operator=(FunctionRef&&) noexcept = delete;
+
+	template <typename... Ts>
+	Result operator()(Ts&&... Args) const
+	{
+		return Function(Object, std::forward<Ts>(Args)...);
+	}
+};
+
+template <typename Result, typename... ArgTypes>
+FunctionRef(Result(*)(ArgTypes...)) -> FunctionRef<Result(ArgTypes...)>;
