46f04cf84a466bcbda67f17ab9fa93380495b6b1
Andrew Depke <andrewdepke@gmail.com>
2020-01-01 01:35:36 -0700

    Updated Copyright Year

--- VanguardEngine/Source/Core/Assert.h
+++ VanguardEngine/Source/Core/Assert.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2019 Andrew Depke
+// Copyright (c) 2019-2020 Andrew Depke
 
 #pragma once
 
--- VanguardEngine/Source/Rendering/RenderSystems.cpp
+++ VanguardEngine/Source/Rendering/RenderSystems.cpp
@@ -1,3 +1,3 @@
-// Copyright (c) 2019 Andrew Depke
+// Copyright (c) 2019-2020 Andrew Depke
 
 #include <Rendering/RenderSystems.h>
