fe0d6e352c3034cb87ebaebadea5e86aa5255673
Andrew Depke <andrewdepke@gmail.com>
2020-05-08 18:28:40 -0600

    Added Cube Model

--- VanguardEngine/Source/Core/Engine.cpp
+++ VanguardEngine/Source/Core/Engine.cpp
@@ -77,13 +77,11 @@ void EngineLoop()
 	entt::registry TempReg;
 
 	TransformComponent SpectatorTransform{};
-	SpectatorTransform.Translation.x = 0.f;
+	SpectatorTransform.Translation.x = -5.f;
 	SpectatorTransform.Translation.y = 0.f;
-	SpectatorTransform.Translation.z = -3.f;
+	SpectatorTransform.Translation.z = 4.f;
 
 	CameraComponent SpectatorCamera{};
-	SpectatorCamera.FocusPosition = { 0.f, 0.f, 0.f };
-	SpectatorCamera.UpDirection = { 0.f, 1.f, 0.f };
 
 	const auto Spectator = TempReg.create();
 	TempReg.assign<TransformComponent>(Spectator, std::move(SpectatorTransform));
@@ -91,10 +89,37 @@ void EngineLoop()
 
 	for (int Index = 0; Index < 1; ++Index)
 	{
-		Vertex v1{ { 0.f, 0.5f, 0.f }, { 1.f, 0.f, 0.f, 1.f } };
-		Vertex v2{ { 0.5f, -0.5f, 0.f }, { 0.f, 1.f, 0.f, 1.f } };
-		Vertex v3{ { -0.5f, -0.5f, 0.f }, { 0.f, 0.f, 1.f, 1.f } };
-		auto Mesh{ CreateMeshComponent(*Renderer::Get().Device, std::vector<Vertex>{ v1, v2, v3 }, std::vector<uint32_t>{ 0, 1, 2 }) };
+		Vertex v0{ { -1.f, -1.f, 1.f }, { 0.4f, 0.f, 0.f, 1.f } };
+		Vertex v1{ { 1.f, -1.f, 1.f }, { 0.f, 0.4f, 0.f, 1.f } };
+		Vertex v2{ { 1.f, 1.f, 1.f }, { 0.f, 0.f, 0.4f, 1.f } };
+		Vertex v3{ { -1.f, 1.f, 1.f }, { 0.f, 0.2f, 0.f, 1.f } };
+
+		Vertex v4{ { -1.f, -1.f, -1.f }, { 0.f, 0.4f, 0.f, 1.f } };
+		Vertex v5{ { -1.f, 1.f, -1.f }, { 0.4f, 0.f, 0.f, 1.f } };
+		Vertex v6{ { 1.f, 1.f, -1.f }, { 0.f, 0.f, 0.4f, 1.f } };
+		Vertex v7{ { 1.f, -1.f, -1.f }, { 0.2f, 0.f, 0.f, 1.f } };
+
+		Vertex v8{ { -1.f, 1.f, -1.f }, { 0.f, 0.4f, 0.f, 1.f } };
+		Vertex v9{ { -1.f, 1.f, 1.f }, { 0.4f, 0.f, 0.f, 1.f } };
+		Vertex v10{ { 1.f, 1.f, 1.f }, { 0.f, 0.f, 0.4f, 1.f } };
+		Vertex v11{ { 1.f, 1.f, -1.f }, { 0.f, 0.2f, 0.f, 1.f } };
+
+		Vertex v12{ { -1.f, -1.f, -1.f }, { 0.f, 0.f, 0.4f, 1.f } };
+		Vertex v13{ { 1.f, -1.f, -1.f }, { 0.f, 0.4f, 0.f, 1.f } };
+		Vertex v14{ { 1.f, -1.f, 1.f }, { 0.4f, 0.f, 0.f, 1.f } };
+		Vertex v15{ { -1.f, -1.f, 1.f }, { 0.f, 0.f, 0.2f, 1.f } };
+
+		Vertex v16{ { 1.f, -1.f, -1.f }, { 0.4f, 0.f, 0.f, 1.f } };
+		Vertex v17{ { 1.f, 1.f, -1.f }, { 0.f, 0.f, 0.4f, 1.f } };
+		Vertex v18{ { 1.f, 1.f, 1.f }, { 0.f, 0.4f, 0.f, 1.f } };
+		Vertex v19{ { 1.f, -1.f, 1.f }, { 0.2f, 0.f, 0.f, 1.f } };
+
+		Vertex v20{ { -1.f, -1.f, -1.f }, { 0.f, 0.4f, 0.f, 1.f } };
+		Vertex v21{ { -1.f, -1.f, 1.f }, { 0.4f, 0.f, 0.f, 1.f } };
+		Vertex v22{ { -1.f, 1.f, 1.f }, { 0.f, 0.f, 0.4f, 1.f } };
+		Vertex v23{ { -1.f, 1.f, -1.f }, { 0.f, 0.2f, 0.f, 1.f } };
+
+		auto Mesh{ CreateMeshComponent(*Renderer::Get().Device, std::vector<Vertex>{ v0,v1,v2,v3,v4,v5,v6,v7,v8,v9,v10,v11,v12,v13,v14,v15,v16,v17,v18,v19,v20,v21,v22,v23 }, std::vector<uint32_t>{ 0, 1, 2, 0, 2, 3, 4, 5, 6, 4, 6, 7, 8, 9, 10, 8, 10, 11, 12, 13, 14, 12, 14, 15, 16, 17, 18, 16, 18, 19, 20, 21, 22, 20, 22, 23 }) };
 
 		const auto Entity = TempReg.create();
 		TempReg.assign<TransformComponent>(Entity);
