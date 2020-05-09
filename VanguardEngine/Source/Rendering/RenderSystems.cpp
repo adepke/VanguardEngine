74c47fae2462e5bad65c866b2462d153e3a6b0b9
Andrew Depke <andrewdepke@gmail.com>
2020-05-08 18:28:23 -0600

    Hardcoded Up Direction, Removed Transpose, Pack Shaders for Row Major

--- VanguardEngine/Source/Rendering/RenderSystems.cpp
+++ VanguardEngine/Source/Rendering/RenderSystems.cpp
@@ -14,14 +14,14 @@ void CameraSystem::Update(entt::registry& Registry)
 		{
 			const auto EyePosition = XMVectorSet(Transform.Translation.x, Transform.Translation.y, Transform.Translation.z, 0.f);
 			const auto FocusPosition = XMVectorSet(Camera.FocusPosition.x, Camera.FocusPosition.y, Camera.FocusPosition.z, 0.f);
-			const auto UpDirection = XMVectorSet(Camera.UpDirection.x, Camera.UpDirection.y, Camera.UpDirection.z, 0.f);
+			const auto UpDirection = XMVectorSet(0.f, 0.f, 1.f, 0.f);  // Z-up.
 			const auto AspectRatio = static_cast<float>(Renderer::Get().Device->RenderWidth) / static_cast<float>(Renderer::Get().Device->RenderHeight);
 
 			const auto ViewMatrix = XMMatrixLookAtRH(EyePosition, FocusPosition, UpDirection);
 			const auto ProjectionMatrix = XMMatrixPerspectiveFovRH(Camera.FieldOfView / 2.f, AspectRatio, Camera.NearPlane, Camera.FarPlane);
-
+			
 			// #TODO: Support multiple cameras.
-			GlobalViewMatrix = XMMatrixTranspose(ViewMatrix);
-			GlobalProjectionMatrix = XMMatrixTranspose(ProjectionMatrix);
+			GlobalViewMatrix = ViewMatrix;
+			GlobalProjectionMatrix = ProjectionMatrix;
 		});
 }
