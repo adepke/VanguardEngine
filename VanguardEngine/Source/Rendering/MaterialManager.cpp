6e6b12bafd61601cd9744ffad39c894df4867d67
Andrew Depke <andrewdepke@gmail.com>
2020-05-24 03:25:00 -0600

    Use Triangle Lists Instead of Strips

--- VanguardEngine/Source/Rendering/MaterialManager.cpp
+++ VanguardEngine/Source/Rendering/MaterialManager.cpp
@@ -76,7 +76,7 @@ std::vector<Material> MaterialManager::ReloadMaterials(RenderDevice& Device)
 		Desc.DepthStencilDescription.BackFace.StencilDepthFailOp = D3D12_STENCIL_OP_KEEP;
 		Desc.DepthStencilDescription.BackFace.StencilPassOp = D3D12_STENCIL_OP_KEEP;
 		Desc.DepthStencilDescription.BackFace.StencilFunc = D3D12_COMPARISON_FUNC_ALWAYS;
-		Desc.Topology = D3D_PRIMITIVE_TOPOLOGY_TRIANGLESTRIP;
+		Desc.Topology = D3D_PRIMITIVE_TOPOLOGY_TRIANGLELIST;
 
 		NewMat.Pipeline = std::make_unique<PipelineState>();
 		NewMat.Pipeline->Build(Device, Desc);  // #TODO: Pipeline libraries.
