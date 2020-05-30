2cd770129243a9f3ecf0fbe1e164ce5b8c38d79f
Andrew Depke <andrewdepke@gmail.com>
2020-05-29 18:33:23 -0600

    Added Placeholder Descriptor Table Generation

--- VanguardEngine/Source/Rendering/DescriptorAllocator.h
+++ VanguardEngine/Source/Rendering/DescriptorAllocator.h
@@ -10,6 +10,20 @@
 #include <array>
 #include <vector>
 
+enum class DescriptorTableEntryType
+{
+	ShaderResource,
+	UnorderedAccess,
+	ConstantBuffer,
+	Sampler,
+};
+
+struct PendingDescriptorTableEntry
+{
+	D3D12_CPU_DESCRIPTOR_HANDLE Handle;
+	DescriptorTableEntryType Type;
+};
+
 class RenderDevice;
 
 class DescriptorAllocator
@@ -25,12 +39,17 @@ private:
 	FreeQueueDescriptorHeap RenderTargetHeap;
 	FreeQueueDescriptorHeap DepthStencilHeap;
 
+	std::vector<PendingDescriptorTableEntry> PendingTableEntries;
+
 public:
 	void Initialize(RenderDevice* InDevice, size_t OfflineDescriptors, size_t OnlineDescriptors);
 
 	DescriptorHandle Allocate(DescriptorType Type);
 
-	//void BuildTable(...);
+	void AddTableEntry(DescriptorHandle& Handle, DescriptorTableEntryType Type);
+
+	// Constructs the pending descriptor table and copies descriptors to an online heap.
+	void BuildTable(CommandList& List, uint32_t RootParameter);
 	
 	void FrameStep(size_t FrameIndex);
 };
