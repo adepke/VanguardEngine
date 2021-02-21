82ffcaa668fb9187e7887b52a81b206ecc9a7aaf
Andrew Depke <andrewdepke@gmail.com>
2021-02-20 20:44:27 -0700

    Added sRGB format query

--- VanguardEngine/Source/Rendering/ResourceFormat.h
+++ VanguardEngine/Source/Rendering/ResourceFormat.h
@@ -119,6 +119,23 @@ uint32_t GetResourceFormatSize(DXGI_FORMAT format)
 	return 0;
 }
 
+bool IsResourceFormatSRGB(DXGI_FORMAT format)
+{
+	switch (format)
+	{
+	case DXGI_FORMAT_R8G8B8A8_UNORM_SRGB:
+	case DXGI_FORMAT_BC1_UNORM_SRGB:
+	case DXGI_FORMAT_BC2_UNORM_SRGB:
+	case DXGI_FORMAT_BC3_UNORM_SRGB:
+	case DXGI_FORMAT_B8G8R8A8_UNORM_SRGB:
+	case DXGI_FORMAT_B8G8R8X8_UNORM_SRGB:
+	case DXGI_FORMAT_BC7_UNORM_SRGB:
+		return true;
+	default:
+		return false;
+	}
+}
+
 DXGI_FORMAT ConvertResourceFormatToSRGB(DXGI_FORMAT linearFormat)
 {
 	switch (linearFormat)
