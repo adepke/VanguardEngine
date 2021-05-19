6f8f298f7f340cb50e14e9e9447b5c65990a9a28
Andrew Depke <andrewdepke@gmail.com>
2021-05-19 12:06:30 -0600

    Added Agility SDK, closes #47

---
+++ VanguardEngine/Source/Core/Exports.cpp
@@ -0,0 +1,10 @@
+// Copyright (c) 2019-2021 Andrew Depke
+
+#include <cstdint>
+
+extern "C"
+{
+	// DirectX Agility SDK
+	__declspec(dllexport) extern const uint32_t D3D12SDKVersion = 4;
+	__declspec(dllexport) extern const char* D3D12SDKPath = ".\\D3D12\\";
+};
