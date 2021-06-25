53ae7779e5aef42a626b8703868edf38205bd401
Andrew Depke <andrewdepke@gmail.com>
2021-06-24 22:03:05 -0600

    Added UAV clearing compute shader

---
+++ VanguardEngine/Source/Rendering/RenderUtils.h
@@ -0,0 +1,23 @@
+// Copyright (c) 2019-2021 Andrew Depke
+
+#pragma once
+
+#include <Rendering/Base.h>
+#include <Utility/Singleton.h>
+#include <Rendering/PipelineState.h>
+#include <Rendering/ResourceHandle.h>
+
+class RenderDevice;
+class CommandList;
+
+class RenderUtils : public Singleton<RenderUtils>
+{
+private:
+	RenderDevice* device = nullptr;
+	PipelineState clearUAVState;
+
+public:
+	void Initialize(RenderDevice* inDevice);
+
+	void ClearUAV(CommandList& list, BufferHandle buffer);
+};
