f3936d862f2d66aa32257ce8994bab721ac901f6
Andrew Depke <andrewdepke@gmail.com>
2022-04-22 15:12:50 -0600

    Fixed frustum culling

--- VanguardEngine/Shaders/MeshCulling.hlsl
+++ VanguardEngine/Shaders/MeshCulling.hlsl
@@ -27,33 +27,35 @@ struct MeshIndirectArgument
 	float2 padding;
 };
 
-float4 normalizePlane(float4 p)
-{
-	return p / length(p.xyz);
-}
-
 bool IsVisible(ObjectData object, Camera camera)
 {
 	float3 center = float3(object.worldMatrix._m30, object.worldMatrix._m31, object.worldMatrix._m32);
-	float radius = object.boundingSphereRadius * 10.f;
+	float radius = object.boundingSphereRadius * 4.f;  // #TODO: Something weird with bounding sphere size...
 
 	// Project sphere into view space.
-	center = mul(float4(center, 1.f), camera.view).xyz;
-	center.z = -center.z;
+	float4 viewSpace = mul(float4(center, 1.f), camera.view);
+	center = (viewSpace / viewSpace.w).xyz;  // Perspective division.
 
-	matrix projectionTranspose = camera.projection;//transpose(camera.projection);
+	matrix projectionTranspose = transpose(camera.projection);
 	float4 r0 = float4(projectionTranspose._m00, projectionTranspose._m01, projectionTranspose._m02, projectionTranspose._m03);
 	float4 r1 = float4(projectionTranspose._m10, projectionTranspose._m11, projectionTranspose._m12, projectionTranspose._m13);
+	float4 r2 = float4(projectionTranspose._m20, projectionTranspose._m21, projectionTranspose._m22, projectionTranspose._m23);
 	float4 r3 = float4(projectionTranspose._m30, projectionTranspose._m31, projectionTranspose._m32, projectionTranspose._m33);
-	float4 frustumX = normalizePlane(r3 + r0);
-	float4 frustumY = normalizePlane(r3 + r1);
+
+	// https://fgiesen.wordpress.com/2012/08/31/frustum-planes-from-the-projection-matrix/
+	// Frustum plane inequalities:
+	// -w <= x <= w
+	// -w <= y <= w
+	// 0 <= z <= w
+	// #TODO: More efficient frustum culling: https://github.com/zeux/niagara/blob/master/src/shaders/drawcull.comp.glsl
 
 	bool visible = true;
-
-	// Frustum culling. Credit: https://github.com/zeux/niagara/blob/master/src/shaders/drawcull.comp.glsl
-	visible = visible && center.z * frustumX.z - abs(center.x) * frustumX.x > -radius;  // Left/right planes.
-	visible = visible && center.z * frustumY.z - abs(center.y) * frustumY.y > -radius;  // Top/bottom planes.
-	visible = visible && center.z + radius > camera.nearPlane && center.z - radius < camera.farPlane;  // Near/far planes.
+	visible = visible && -radius <= mul(center, r3 + r0);  // Left plane
+	visible = visible && -radius <= mul(center, r3 - r0);  // Right plane
+	visible = visible && -radius <= mul(center, r3 + r1);  // Bottom plane
+	visible = visible && -radius <= mul(center, r3 - r1);  // Top plane
+	visible = visible && -radius <= mul(center, r3);  // Near plane
+	visible = visible && -radius <= mul(center, r3 - r2);  // Far plane
 
 	return visible;
 }
@@ -71,7 +73,7 @@ void Main(uint dispatchId : SV_DispatchThreadID)
 	uint index = dispatchId.x;
 	if (index < bindData.drawCount)
 	{
-		uint objectId = inputBuffer[index].batchId;  // #TEMP: Sufficient for now, won't work with instancing.
+		uint objectId = inputBuffer[index].batchId;  // #TODO: Sufficient for now, won't work with instancing.
 		ObjectData object = objectBuffer[objectId];
 		if (IsVisible(object, camera))
 		{
