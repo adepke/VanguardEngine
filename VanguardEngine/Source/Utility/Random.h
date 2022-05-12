454b401fbbd91380e5bd70bb2cbb8ee09003bd23
Andrew Depke <andrewdepke@gmail.com>
2022-05-12 15:25:50 -0600

    Fixed ODR

--- VanguardEngine/Source/Utility/Random.h
+++ VanguardEngine/Source/Utility/Random.h
@@ -6,20 +6,20 @@
 
 namespace Detail
 {
-	auto thread_local generator = std::mt19937_64{};
+	inline auto thread_local generator = std::mt19937_64{};
 }
 
-void Seed(std::seed_seq seed)
+inline void Seed(std::seed_seq seed)
 {
 	Detail::generator.seed(seed);
 }
 
-int Rand(int min, int max)
+inline int Rand(int min, int max)
 {
 	return std::uniform_int_distribution{ min, max }(Detail::generator);
 }
 
-double Rand(double min, double max)
+inline double Rand(double min, double max)
 {
 	return std::uniform_real_distribution{ min, max }(Detail::generator);
 }
